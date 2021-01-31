bf4ecfe2e6332c7c88bdba3201aa7ca7d7e760ef
LAH1203 <lah1203@naver.com>
2021-01-31 14:04:52 +0900

    Update mysql user

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-var express = require('express')
-var mysql = require('mysql')
-var location = require('location')
+var express = require('express');
+var mysql = require('mysql');
+var location = require('location');
 var conn = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    // 실행 시마다 패스워드 변경하도록 하자..
-    // password: 'password',
+    user: 'WhereGo',
+    password: 'password',
     database: 'reviews'
-})
-conn.connect()
-var bodyParser = require('body-parser')
-var app = express()
-const { auth, requiresAuth } = require('express-openid-connect')
-const { requireAuth } = require('express-openid-connect')
+});
+conn.connect();
+var bodyParser = require('body-parser');
+var app = express();
+const { auth, requiresAuth } = require('express-openid-connect');
+const { requireAuth } = require('express-openid-connect');
 const config = {
     authRequired: false,
     auth0Logout: true,
@@ -22,118 +21,119 @@ const config = {
     issuerBaseURL: 'https://lah1203.eu.auth0.com'
 }
 
-app.locals.pretty = true
-app.set('views', './views')
-app.set('view engine', 'jade')
+app.locals.pretty = true;
+app.set('views', './views');
+app.set('view engine', 'jade');
 // 정적인 파일이 위치할 디렉토리 지정
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 // auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config))
+app.use(auth(config));
 
 // req.isAuthenticated is provided from the auth router
 // Auth0에서 제공하는 로그인 및 회원가입 구현 완료!
 app.get('/', (req, res) => {
-    // res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
-    res.render(req.oidc.isAuthenticated() ? 'login_success' : 'login_fail')
-})
+    // res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+    res.render(req.oidc.isAuthenticated() ? 'login_success' : 'login_fail');
+});
 
 app.get('/logout', (req, res) => {
-    res.render(req.oidc.auth0Logout() ? 'logout_success' : 'logout_fail')
-})
+    res.render(req.oidc.auth0Logout() ? 'logout_success' : 'logout_fail');
+});
 
 app.get('/profile', requiresAuth(), (req, res) => {
-    // res.send(JSON.stringify(req.oidc.user))
-    // res.send(req.oidc.isAuthenticated() ? JSON.stringify(req.oidc.user) : '로그인 되어있지 않음')
-    res.render(req.oidc.isAuthenticated() ? 'my_page' : 'not_logined', { user_profile_json: req.oidc.user })
-})
+    // res.send(JSON.stringify(req.oidc.user));
+    // res.send(req.oidc.isAuthenticated() ? JSON.stringify(req.oidc.user) : '로그인 되어있지 않음');
+    res.render(req.oidc.isAuthenticated() ? 'my_page' : 'not_logined', { user_profile_json: req.oidc.user });
+});
 
 // 메인
 app.get('/main', function(req, res) {
-    res.render('main_page')
-})
+    res.render('main_page');
+});
 
 // 검색
 app.get('/search', function(req, res) {
-    res.render('search_page')
-})
+    res.render('search_page');
+});
 
-app.get('/searchAfter', function(req, res) {
-    var body = req.body
-    var sql = 'SELECT * FROM review WHERE PLACE=?'
-    var params = [body.place]
+app.post('/searchAfter', function(req, res) {
+    var body = req.body;
+    var sql = 'SELECT * FROM review WHERE PLACE=?';
+    var params = [body.place];
     conn.query(sql, params, function(err, rows) {
         if (rows == 0) {
-            res.render('no_search_result')
+            res.render('no_search_result');
         }
         else if (err) {
-            console.log('query is not excuted. select fail...\n' + err)
-            res.render('search_fail')
+            console.log('query is not excuted. select fail...\n' + err);
+            res.render('search_fail');
         } else {
-            res.render('search_success', { result: rows })
+            res.render('search_success', { result: rows });
         }
-    })
-})
+    });
+});
 
 // 글쓰기
 app.get('/new', function(req, res) {
-    // res.render('new_page')
-    res.render(req.oidc.isAuthenticated() ? 'new_page' : 'not_logined')
-})
+    // res.render('new_page');
+    res.render(req.oidc.isAuthenticated() ? 'new_page' : 'not_logined');
+});
 
 app.post('/newAfter', function(req, res) {
-    var body = req.body
+    var body = req.body;
     
-    var sql = 'INSERT INTO review (place, content, user_email) VALUES(?, ?, ?)'
-    var params = [body.place, body.content, JSON.stringify(req.oidc.user.email).toString().replace(/"/g, "")]
+    var sql = 'INSERT INTO review (place, content, user_email) VALUES(?, ?, ?)';
+    var params = [body.place, body.content, JSON.stringify(req.oidc.user.email).toString().replace(/"/g, "")];
     conn.query(sql, params, function(err) {
         if (err) {
-            console.log('query is not excuted. insert fail...\n' + err)
-            res.render('new_fail')
+            console.log('query is not excuted. insert fail...\n' + err);
+            res.render('new_fail');
         } else {
-            res.render('new_success')
+            res.render('new_success');
         }
-    })
-})
+    });
+});
 
 // 관광코스
 app.get('/course', function(req, res) {
-    var course_id = req.query.course_id
-    var link = null
+    var course_id = req.query.course_id;
+    var link = null;
     if (course_id != null) {
-        var url = 'http://apis.data.go.kr/1360000/TourStnInfoService/getTourStnVilageFcst'
-        var queryParams = '?serviceKey=M%2BsXx7%2BQWCiCOcTgFAo0V4vPG%2Fi3dmWlPDWY0tEmEQB44%2Fdt%2FHgAsCR%2FcewDOVDRTTPG0IM7rujX0YdJ%2BzV9Xw%3D%3D&numOfRows=10&pageNo=1&CURRENT_DATE=2019122010&HOUR=24'
-        queryParams += '&COURSE_ID=' + course_id
-        link = url + queryParams
-        // res.writeHead(302, {'Location': link})
-        // res.end()
-        location.href = link    // 오류
+        var url = 'http://apis.data.go.kr/1360000/TourStnInfoService/getTourStnVilageFcst';
+        var queryParams = '?serviceKey=M%2BsXx7%2BQWCiCOcTgFAo0V4vPG%2Fi3dmWlPDWY0tEmEQB44%2Fdt%2FHgAsCR%2FcewDOVDRTTPG0IM7rujX0YdJ%2BzV9Xw%3D%3D&numOfRows=10&pageNo=1&CURRENT_DATE=2019122010&HOUR=24';
+        queryParams += '&COURSE_ID=' + course_id;
+        link = url + queryParams;
+        // res.writeHead(302, {'Location': link});
+        // res.end();
+        location.href = link;    // 오류
     }
-    res.render('course_page', { link: link })
-})
+    res.render('course_page', { link: link });
+});
 
+/*
 // 로그인
 app.get('/login', function(req, res) {
-    res.render('login_page')
-})
+    res.render('login_page');
+});
 
 app.get('/login_success', function(req, res) {
-    res.render('login_success')
-})
+    res.render('login_success');
+});
 
 // 회원가입
 app.get('/signup', function(req, res) {
-    res.render('signup_page')
-})
+    res.render('signup_page');
+});
 
 app.get('/signup_success', function(req, res) {
-    res.render('signup_success')
-})
+    res.render('signup_success');
+});
 
 app.get('/signup_fail', function(req, res) {
-    res.render('signup_fail')
-})
-
+    res.render('signup_fail');
+});
+*/
 
 app.listen(3000, function() {
     console.log('Connected, 3000 port!')
